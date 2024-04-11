b5a08b61189cdc4af40ab2f26d49114e0ff98e6b
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-11 23:33:24 +0200

    Use some basic event such as the ready event and the bot is now able to run!

--- src/main.py
+++ src/main.py
@@ -1,2 +1,23 @@
 import os
-print(os.getenv("BOT_TOKEN"))
+import discord
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+client.run(BOT_TOKEN)
