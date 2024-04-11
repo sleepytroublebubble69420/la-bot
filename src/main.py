687908b8c24cfcd8e40288b6998e1d47be48b386
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-12 00:00:51 +0200

    Wrapped discord client inside a class

--- src/main.py
+++ src/main.py
@@ -1,23 +1,19 @@
 import os
 import discord
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
-
+client = MyClient(intents=intents)
 client.run(BOT_TOKEN)
