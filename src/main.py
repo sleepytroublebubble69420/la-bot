56a1860b92568429d2c4b616924070b8c328bc6a
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-12 05:10:38 +0200

    Trying to figure out application commands

--- src/main.py
+++ src/main.py
@@ -1,19 +1,32 @@
 import os
-import discord
+from discord import Client, Intents, Object, Interaction
+from discord.app_commands import CommandTree
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DEBUG_GUILD_ID = os.getenv("DEBUG_GUILD_ID")
 
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
-
-intents = discord.Intents.default()
+intents = Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(BOT_TOKEN)
+client = Client(intents=intents)
+command_tree = CommandTree(client)
+
+
+# @command_tree.command(
+#     name="ping",
+#     description="sends \"pong\"",
+# )
+# async def gping(interaction: Interaction):
+#     await interaction.response.send_message("pong")
+
+
+@client.event
+async def on_ready():
+    # command_tree.clear_commands(guild=Object(id=DEBUG_GUILD_ID))
+    print(len(command_tree.get_commands()))
+    await command_tree.sync(guild=Object(id=DEBUG_GUILD_ID))
+    print("Ready!")
+
+
+if __name__ == "__main__":
+    client.run(BOT_TOKEN)
