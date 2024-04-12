fcf5c53380d3c141fdd3effd06515b2a68d06736
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-13 01:41:45 +0200

    Trying to understand how to implement hybrid commands, implement a message command to sync the command tree

--- src/la_bot/bot.py
+++ src/la_bot/bot.py
@@ -1,13 +1,23 @@
+from discord import Object
 from discord.ext import commands
-from discord.ext.commands import command, Context
+from discord.ext.commands import hybrid_command, Context
 
 
 class Bot(commands.Bot):
     async def on_ready(self):
         self.add_command(close)
+        self.add_command(sync_tree)
         print("Ready!")
 
 
-@command()
+@hybrid_command()
 async def close(ctx: Context):
     await ctx.bot.close()
+
+
+@hybrid_command()
+async def sync_tree(ctx: Context):
+    guild_id = ctx.guild.id
+    print(f"Trying to sync tree for guild (id = {guild_id})")
+    await ctx.bot.tree.sync(guild=Object(id=guild_id))
+    print(f"Tree should be synced for guild (id = {guild_id})")
