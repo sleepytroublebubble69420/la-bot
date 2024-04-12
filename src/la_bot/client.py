52777495b8f9c517e359d4ec06d3594f35ba71d7
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-12 16:13:05 +0200

    Succesfully subclassed command tree, created a command and add it to the tree in command_tree.py, also printed the process for debugging purposes inside client.py

--- src/la_bot/client.py
+++ src/la_bot/client.py
@@ -5,7 +5,8 @@ from .command_tree import CommandTree
 class Client(discord.Client):
     async def on_ready(self):
         command_tree = CommandTree(self)
-        command_tree.clear_commands(guild=None)
-        await command_tree.sync(guild=None)
+        app_commands = await command_tree.sync(guild=None)
+        print("Command tree should be synced")
+        print(app_commands)
 
         print("Ready!")
--- src/la_bot/command_tree.py
+++ src/la_bot/command_tree.py
@@ -1,5 +1,16 @@
 import discord
+from discord import Interaction
+from discord.app_commands import command
 
 
 class CommandTree(discord.app_commands.CommandTree):
-    pass
+    def __init__(self, client):
+        super().__init__(client)
+        self.add_command(ping)
+        print("Command Tree initialised")
+
+
+@command()
+async def ping(interaction: Interaction):
+    """Respond with a \"pong!\""""
+    await interaction.response.send_message("pong")
