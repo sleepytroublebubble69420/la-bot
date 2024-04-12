e4f56d36cbd2bb9f6a359c95e776bb6f0f55d8dd
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-12 16:51:13 +0200

    Now uses discord.ext.commands.Bot instead of discord.Client for message command

--- src/la_bot/bot.py
+++ src/la_bot/bot.py
@@ -1,14 +1,8 @@
-import discord
 from discord.ext import commands
 from .command_tree import CommandTree
 
 
-class Client(discord.Client):
+class Bot(commands.Bot):
     async def on_ready(self):
         command_tree = CommandTree(self)
-
-        app_commands = await command_tree.sync(guild=None)
-        print("Command tree should be synced")
-        print(app_commands)
-
         print("Ready!")
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import os
-from la_bot.client import Client
+from la_bot.bot import Bot
 from discord import Intents
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     intents = Intents.default()
     intents.message_content = True
 
-    la_bot = Client(intents=intents)
+    la_bot = Bot(intents=intents)
     la_bot.run(BOT_TOKEN)
