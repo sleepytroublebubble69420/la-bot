c30e6f51ba00406f6c05ad0fed77ef7039a4f4ec
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-12 14:55:09 +0200

    Renamed LaBot class to (la_bot.)Client

--- src/la_bot/client.py
+++ src/la_bot/client.py
@@ -1,8 +1,8 @@
-from discord import Client
+import discord
 from discord.app_commands import CommandTree
 
 
-class LaBot(Client):
+class Client(discord.Client):
     async def on_ready(self):
         command_tree = CommandTree(self)
         command_tree.clear_commands(guild=None)
