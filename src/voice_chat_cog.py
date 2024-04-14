2a7ff65f434908598c3df2b56211bcbcffc5bee3
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-14 22:14:17 +0200

    Corrected a missing "self" argument inside a method

--- src/voice_chat_cog.py
+++ src/voice_chat_cog.py
@@ -3,6 +3,6 @@ from discord.ext.commands import Cog, command, Context
 
 class VoiceChatCog(Cog):
     @command(name="vc_create")
-    async def create_voice_chat(ctx: Context, name):
+    async def create_voice_chat(self, ctx: Context, name):
         await ctx.send("should create voice chat")
         await ctx.guild.create_voice_channel(name)
