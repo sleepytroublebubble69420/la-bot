1987e1efd298bce8258d08b840f8b82c9efdb6dd
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-14 22:11:36 +0200

    Renamed command to a name with no spaces because only the first word is considered

--- src/voice_chat_cog.py
+++ src/voice_chat_cog.py
@@ -2,7 +2,7 @@ from discord.ext.commands import Cog, command, Context
 
 
 class VoiceChatCog(Cog):
-    @command(name="vc create")
+    @command(name="vc_create")
     async def create_voice_chat(ctx: Context, name):
         await ctx.send("should create voice chat")
         await ctx.guild.create_voice_channel(name)
