c8c9a3372ed91f084465d765eab3b3998a6fcfac
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-14 13:58:14 +0200

    Changed bot to not use my subclass of command tree (I'm trying to make hybrid command appear in context menu)

--- src/main.py
+++ src/main.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     intents = Intents.default()
     intents.message_content = True
 
-    la_bot = Bot("la_bot/", intents=intents, tree_cls=CommandTree)
+    la_bot = Bot("la_bot/", intents=intents)  # , tree_cls=CommandTree)
     la_bot.run(BOT_TOKEN)
