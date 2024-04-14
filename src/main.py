0e6753bd3079bea52cae5e245c2d23da387f58d8
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-14 18:42:32 +0200

    Set up logging

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import os
+import logging
 from bot import Bot
 from discord import Intents
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if __name__ == "__main__":
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+    handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+
     intents = Intents.default()
     intents.message_content = True
 
     la_bot = Bot("la_bot/", intents=intents)
-    la_bot.run(BOT_TOKEN)
+    la_bot.run(BOT_TOKEN, log_handler=handler, log_level=logging.DEBUG)
