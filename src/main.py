9d87c034625409c987f42454ac05ed1a2b79b544
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-14 15:11:12 +0200

    Removed mentions of, now deleted, command tree subclass

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import os
 from la_bot.bot import Bot
-from la_bot.command_tree import CommandTree
 from discord import Intents
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -9,5 +8,5 @@ if __name__ == "__main__":
     intents = Intents.default()
     intents.message_content = True
 
-    la_bot = Bot("la_bot/", intents=intents)  # , tree_cls=CommandTree)
+    la_bot = Bot("la_bot/", intents=intents)
     la_bot.run(BOT_TOKEN)
